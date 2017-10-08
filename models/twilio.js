7cb3e89240d0c6f9130748fd29466a80d6cc46a3
Nathan Bernabe <nbernabe09@aol.com>
2017-10-08 12:39:53 -0700

    Added quick AJAX call to AccuWeather with possible Widget

--- models/twilio.js
+++ models/twilio.js
@@ -3,11 +3,11 @@ var keys = require("../config/keys.js");
 var twilio = require('twilio');
 
 // Find your account sid and auth token in your Twilio account Console.
-var client = new twilio(keys.TWILIO_ACCOUNT_SID, keys.TWILIO_AUTH_TOKEN);
+var client = new twilio(keys.info.twilioSID, keys.info.twilioToken);
 
 // Send the text message.
 client.messages.create({
-  to: keys.to,
-  from: keys.from,
+  to: keys.info.verifiedNum,
+  from: keys.info.twilioNum,
   body: 'Hello from Twilio!'
 });
