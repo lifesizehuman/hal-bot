8aabae139f12d0b7310446ffa81f467372db98d8
Slater Combes <combes.slater@gmail.com>
2017-10-10 18:00:58 -0700

    api routes file (base)

---
+++ routes/api-routes.js
@@ -0,0 +1,63 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/query", function(req, res) {
+    // something something
+  }).then(function(results) {
+    res.render(results);
+  }).catch(function(err) {
+    if (err) {
+      res.status(400).end();
+    }
+  });
+
+  app.get("/api/weather", function(req, res) {
+    // something something
+  }).then(function(results) {
+    res.render(results);
+  }).catch(function(err) {
+    if (err) {
+      res.status(400).end();
+    }
+  });
+
+  app.get("/api/twit", function(req, res) {
+    // something something
+  }).then(function(results) {
+    res.render(results);
+  }).catch(function(err) {
+    if (err) {
+      res.status(400).end();
+    }
+  });
+
+  app.get("/api/movie", function(req, res) {
+    // something something
+  }).then(function(results) {
+    res.render(results);
+  }).catch(function(err) {
+    if (err) {
+      res.status(400).end();
+    }
+  });
+
+  app.get("/api/wiki", function(req, res) {
+    // something something
+  }).then(function(results) {
+    res.render(results);
+  }).catch(function(err) {
+    if (err) {
+      res.status(400).end();
+    }
+  });
+
+  app.post("/api/search", function(req, res) {
+    // something something recent searches of population
+  }).then(function(results) {
+    res.render(results);
+  }).catch(function(err) {
+    if (err) {
+      res.status(400).end();
+    }
+  });
+};
