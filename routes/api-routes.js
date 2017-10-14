768d84c8dda718170030cd55854087e728454077
Nathan Bernabe <nbernabe09@aol.com>
2017-10-14 15:34:30 -0700

    Todo code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -119,7 +119,8 @@ module.exports = function(app) {
 
   app.post("/api/addTodo", function(req, res) {
     db.Todo.create({
-      task: req.body.task,// Someplace
+      task: req.body.task,
+      user_ID: req.body.id
     }).then(function() {
       res.end();
     });
