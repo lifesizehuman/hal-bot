6738b417f6f74ef3533f92aa65832ffb1ec2f51a
Nathan Bernabe <nbernabe09@aol.com>
2017-10-14 15:47:48 -0700

    The Force is back

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set("view engine", "handlebars");
 
 require("./routes/api-routes.js")(app);
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
