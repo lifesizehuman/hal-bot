a450df380a1ae9e5d4bf709c006e6ce399a0daa0
Slater Combes <combes.slater@gmail.com>
2017-10-17 12:44:14 -0700

    hail mary

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -5,7 +5,7 @@ window.fbAsyncInit = function() {
   FB.AppEvents.logPageView();
   FB.getLoginStatus(function(response) {
     let userID = response.authResponse.userID;
-    console.log(userID);
+    console.log("FB User ID: " + userID);
     let id = userID;
     $.ajax({
       type: "POST",
@@ -13,108 +13,108 @@ window.fbAsyncInit = function() {
     });
     getTodos();
   });
-};
 
-(function(d, s, id) {
-  let js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
+  (function(d, s, id) {
+    let js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=129221391068505";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  function clearTodos() {
+    $("#todos").empty();
   }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=129221391068505";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
-function clearTodos() {
-  $("#todos").empty();
-}
+  function insertTodo(obj) {
+    let p = $("<p>");
+    p.attr("class", "todo-item");
+    p.attr("contenteditable", true);
+    p.attr("data-id", obj.id);
+    let b = $("<button>");
+    b.attr("type", "button");
+    b.attr("data-id", obj.id);
+    b.attr("class", "close delete-todo");
+    b.attr("aria-label", "Close");
+    let s = $("<span>");
+    s.attr("aria-hidden", "true");
+    s.text("&times;");
+    b.html(s);
+    p.html(b);
+    $("#todos").append(p);
+  }
 
-function insertTodo(obj) {
-  let p = $("<p>");
-  p.attr("class", "todo-item");
-  p.attr("contenteditable", true);
-  p.attr("data-id", obj.id);
-  let b = $("<button>");
-  b.attr("type", "button");
-  b.attr("data-id", obj.id);
-  b.attr("class", "close delete-todo");
-  b.attr("aria-label", "Close");
-  let s = $("<span>");
-  s.attr("aria-hidden", "true");
-  s.text("&times;");
-  b.html(s);
-  p.html(b);
-  $("#todos").append(p);
-}
+  function getTodos(id) {
+    $.ajax({
+      type: "GET",
+      url: "/api/todo/" + id
+    }).then((data) => {
+      clearTodos();
+      data.map((entry) => insertTodo(entry));
+    });
+  }
 
-function getTodos(id) {
-  $.ajax({
-    type: "GET",
-    url: "/api/todo/" + id
-  }).then((data) => {
-    clearTodos();
-    data.map((entry) => insertTodo(entry));
+  function createTodo(task, id) {
+    $.ajax({
+      method: "POST",
+      url: "/api/addTodo",
+      data: {
+        task: task,
+        complete: false,
+        UserId: id
+      }
+    }).then(() => getTodos());
+  }
+
+  function completeTodo(id) {
+    $.ajax({
+      type: "PUT",
+      url: "/api/todo/",
+      data: {
+        complete: true,
+        // UserId: userID, // <<------ corect info??
+        id: id
+      }
+    }).then(() => getTodos());
+  }
+
+  function updateTask(id, task) {
+    $.ajax({
+      type: "PUT",
+      url: "/api/todo/",
+      data: {
+        task: task,
+        // UserId: userID, // <<------ corect info??
+        id: id // <<------ corect info??
+      }
+    }).then(() => getTodos());
+  }
+
+  $("#todo-button").on("click", function(event) {
+    event.preventDefault();
+    let task = $("#todo-input").val();
+    if (!task)
+      return;
+    createTodo(task);
   });
-}
 
-function createTodo(task, id) {
-  $.ajax({
-    method: "POST",
-    url: "/api/addTodo",
-    data: {
-      task: task,
-      complete: false,
-      UserId: id
-    }
-  }).then(() => getTodos());
-}
+  $(document).on("input", ".todo-item", function(event) {
+    event.preventDefault();
+    let task = $(this).val();
+    let id = $(this).attr("data-id");
+    updateTask(id, task);
+  });
 
-function completeTodo(id) {
-  $.ajax({
-    type: "PUT",
-    url: "/api/todo/",
-    data: {
-      complete: true,
-      // UserId: userID, // <<------ corect info??
-      id: id
-    }
-  }).then(() => getTodos());
-}
-
-function updateTask(id, task) {
-  $.ajax({
-    type: "PUT",
-    url: "/api/todo/",
-    data: {
-      task: task,
-      // UserId: userID, // <<------ corect info??
-      id: id // <<------ corect info??
-    }
-  }).then(() => getTodos());
-}
-
-$("#todo-button").on("click", function(event) {
-  event.preventDefault();
-  let task = $("#todo-input").val();
-  if (!task)
-    return;
-  createTodo(task);
-});
-
-$(document).on("input", ".todo-item", function(event) {
-  event.preventDefault();
-  let task = $(this).val();
-  let id = $(this).attr("data-id");
-  updateTask(id, task);
-});
-
-$(document).on("click", 'delete-todo', function(event) {
-  event.preventDefault();
-  let id = $(this).attr("data-id");
-  completeTodo(id);
-});
+  $(document).on("click", 'delete-todo', function(event) {
+    event.preventDefault();
+    let id = $(this).attr("data-id");
+    completeTodo(id);
+  });
+};
 
 // var id;
 // window.fbAsyncInit = function() {
